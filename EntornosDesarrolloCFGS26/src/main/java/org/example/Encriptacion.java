4b17f3780ad716e858b5d0edc76ae7cb83b99a3d
Izan de Geer <izandegeer.ua@gmail.com>
2025-12-03 17:38:43 +0100

    ++

---
+++ EntornosDesarrolloCFGS26/src/main/java/org/example/Encriptacion.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Encriptacion {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+        while (true) {
+            String mensaje = teclado.nextLine();
+        }
+    }
+
+    static void encriptarMensaje(String mensaje) {
+
+    }
+}
